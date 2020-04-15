862f8ffaf46b7e1425d1e6322cf3a3af30adf118
Patrício Martins <eupatriciomartins@gmail.com>
2020-04-15 03:12:19 -0300

    create restrict user login

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+	jwt: {
+		secret: 'd4ff47f1399557d0644efa723ac6cf76',
+		expiresIn: '1d',
+	},
+};
--- backend/src/service/AuthenticateUserService.ts
+++ backend/src/service/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface Request {
@@ -29,9 +30,11 @@ class AuthenticateUserService {
 			throw new Error('Incorrect email/password combination.');
 		}
 
-		const token = sign({}, 'd4ff47f1399557d0644efa723ac6cf76', {
+		const { secret, expiresIn } = authConfig.jwt;
+
+		const token = sign({}, secret, {
 			subject: user.id,
-			expiresIn: '1d',
+			expiresIn,
 		});
 
 		return { user, token };
