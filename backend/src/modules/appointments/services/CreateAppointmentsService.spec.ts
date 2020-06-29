f1fae51cd178ef7e9299606a9912fabcc0d864e6
Patrício Martins <eupatriciomartins@gmail.com>
2020-06-29 03:36:27 -0300

    add: validate routes

--- backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -2,15 +2,20 @@ import AppError from '@shared/errors/AppError';
 
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
 let createAppointment: CreateAppointmentService;
+let fakeNotificationsRepository: FakeNotificationsRepository;
 
 describe('CreateAppointment', () => {
 	beforeEach(() => {
 		fakeAppointmentsRepository = new FakeAppointmentsRepository();
+		fakeNotificationsRepository = new FakeNotificationsRepository();
+
 		createAppointment = new CreateAppointmentService(
 			fakeAppointmentsRepository,
+			fakeNotificationsRepository,
 		);
 	});
 
