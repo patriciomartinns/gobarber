503d18c206b9ca5f3788596805d7d2bdd4139e11
Patrício Martins <eupatriciomartins@gmail.com>
2020-06-29 04:14:53 -0300

    add: add class-transform

--- backend/src/modules/users/infra/http/controllers/ProfileController.ts
+++ backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
 
@@ -10,9 +11,7 @@ export default class ProfileController {
 
 		const user = await showProfile.execute({ user_id });
 
-		delete user.password;
-
-		return response.json(user);
+		return response.json(classToClass(user));
 	}
 
 	public async update(
@@ -34,6 +33,6 @@ export default class ProfileController {
 
 		delete user.password;
 
-		return response.json(user);
+		return response.json(classToClass(user));
 	}
 }
--- backend/src/modules/users/infra/http/controllers/UsersController.ts
+++ backend/src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+
 import CreateUserService from '@modules/users/services/CreateUserService';
 
 export default class UsersController {
@@ -19,6 +21,6 @@ export default class UsersController {
 
 		delete user.password;
 
-		return response.json(user);
+		return response.json(classToClass(user));
 	}
 }
