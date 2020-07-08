8b71493499da83786fa3c2a2c9160cb302d3f60d
Patrício Martins <eupatriciomartins@gmail.com>
2020-07-08 06:05:19 -0300

    add: added adding the application login

---
+++ mobile/src/hooks/auth.tsx
@@ -0,0 +1,84 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+  useEffect,
+} from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
+import api from '../services/api'
+
+interface AuthState {
+  token: string
+  user: object
+}
+
+interface SignInCredentials {
+  email: string
+  password: string
+}
+
+interface AuthContextData {
+  user: object
+  signIn(credentials: SignInCredentials): Promise<void>
+  signOut(): void
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>({} as AuthState)
+
+  useEffect(() => {
+    async function loadStorageData(): Promise<void> {
+      const [user, token] = await AsyncStorage.multiGet([
+        '@GoBarber:user',
+        '@GoBarber:token',
+      ])
+
+      if (user[1] && token[1]) {
+        setData({ user: JSON.parse(user[1]), token: token[1] })
+      }
+    }
+
+    loadStorageData()
+  }, [])
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    })
+
+    const { token, user } = response.data
+
+    await AsyncStorage.multiSet([
+      ['@GoBarber:user', JSON.stringify(user)],
+      ['@GoBarber:token', token],
+    ])
+
+    setData({ token, user })
+  }, [])
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@GoBarber:user', '@GoBarber:token'])
+
+    setData({} as AuthState)
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+
+  return context
+}
--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -13,6 +13,7 @@ import { useNavigation } from '@react-navigation/native'
 import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup'
+import api from '../../services/api'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 
@@ -36,45 +37,48 @@ const SignUp: React.FC = () => {
   const emailInputRef = useRef<TextInput>(null)
   const passwordInputRef = useRef<TextInput>(null)
 
-  const handleSignUp = useCallback(async (data: SignUpFormData) => {
-    try {
-      formRef.current?.setErrors({})
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({})
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string()
-          .required('Email obrigatório')
-          .email('Digite um email válido'),
-        password: Yup.string().required().min(6, 'No mínimo 6 dígitos'),
-      })
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Digite um email válido'),
+          password: Yup.string().required().min(6, 'No mínimo 6 dígitos'),
+        })
 
-      await schema.validate(data, {
-        abortEarly: false,
-      })
+        await schema.validate(data, {
+          abortEarly: false,
+        })
 
-      // await api.post('/users', data)
+        await api.post('/users', data)
 
-      // history.push('/')
+        Alert.alert(
+          'Cadastro realizado!',
+          'Você já pode fazer seu login no GoBarber!',
+        )
 
-      Alert.alert(
-        'Cadastro realizado!',
-        'Você já pode fazer seu login no GoBarber!',
-      )
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err)
+        navigation.navigate('SignIn')
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err)
 
-        formRef.current?.setErrors(errors)
+          formRef.current?.setErrors(errors)
 
-        return
+          return
+        }
+
+        Alert.alert(
+          'Erro no cadastro',
+          'Ocorreu um erro ao fazer o cadastro, tente novamente.',
+        )
       }
-
-      Alert.alert(
-        'Erro no cadastro',
-        'Ocorreu um erro ao fazer o cadastro, tente novamente.',
-      )
-    }
-  }, [])
+    },
+    [navigation],
+  )
 
   return (
     <>
