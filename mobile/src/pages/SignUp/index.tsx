add183ef2ee133c586b19b725ae25645700b9979
Patrício Martins <eupatriciomartins@gmail.com>
2020-07-08 04:18:19 -0300

    add: integrated unform

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {
   View,
   Image,
@@ -8,6 +8,7 @@ import {
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -15,8 +16,10 @@ import Button from '../../components/Button'
 import logoImg from '../../assets/logo.png'
 
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
+import { FormHandles } from '@unform/core'
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
   const navigation = useNavigation()
 
   return (
@@ -36,13 +39,22 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Input name="name" icon="user" placeholder="Nome" />
+            <Form
+              ref={formRef}
+              onSubmit={(data) => {
+                console.log(data)
+              }}
+            >
+              <Input name="name" icon="user" placeholder="Nome" />
 
-            <Input name="email" icon="mail" placeholder="Email" />
+              <Input name="email" icon="mail" placeholder="Email" />
 
-            <Input name="password" icon="lock" placeholder="Senha" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+            </Form>
 
-            <Button onPress={() => {}}>Cadastrar</Button>
+            <Button onPress={() => formRef.current?.submitForm()}>
+              Cadastrar
+            </Button>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
