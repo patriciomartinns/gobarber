a6492ac03ff33b3e5e86cdbc01f6d49cdeb812e9
Patrício Martins <eupatriciomartins@gmail.com>
2020-07-08 03:36:19 -0300

    add: finishing the SignUp page end init routing

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -7,6 +7,7 @@ import {
   Platform,
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -23,6 +24,8 @@ import {
 } from './styles'
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation()
+
   return (
     <>
       <KeyboardAvoidingView
@@ -41,7 +44,7 @@ const SignIn: React.FC = () => {
             </View>
 
             <Input name="email" icon="mail" placeholder="Email" />
-            <Input name="password" icon="lock" placeholder="Password" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
             <Button onPress={() => {}}>Entrar</Button>
 
@@ -52,7 +55,7 @@ const SignIn: React.FC = () => {
         </ScrollView>
       </KeyboardAvoidingView>
 
-      <CreateAccountButton onPress={() => {}}>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
         <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
       </CreateAccountButton>
